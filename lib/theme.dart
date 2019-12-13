d70d69a0443d3f1e6b7199a7de7e3b6f2bb3f7cb
Damodar Lohani <dlohani48@gmail.com>
2019-12-13 06:15:08 +0545

    setup

---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+ThemeData light = ThemeData(
+  brightness: Brightness.light,
+  primarySwatch: Colors.indigo,
+  accentColor: Colors.pink,
+  scaffoldBackgroundColor: Color(0xfff1f1f1)
+);
+
+ThemeData dark = ThemeData(
+  brightness: Brightness.dark,
+  primarySwatch: Colors.indigo,
+  accentColor: Colors.pink,
+);
